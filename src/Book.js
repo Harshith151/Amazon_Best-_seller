1c6cf3eeb7bb50810d2015e75924fe38a3e8faf2
Harshith151 <harshithsavanth151@gmail.com>
2023-09-09 09:34:04 +0530

    Initial commit

---
+++ src/Book.js
@@ -0,0 +1,10 @@
+export const Book = ({ author, title, img, index }) => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <span className="index">#{index + 1}</span>
+    </article>
+  );
+};
---
+++ src/books.js
@@ -0,0 +1,26 @@
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+
+export const books = [
+  {
+    title: "Holly Hardcover",
+    author: "Stephen King",
+    img: img1,
+    id: 1,
+  },
+
+  {
+    title: "Things We Left Behind",
+    author: "Lucy Score",
+    img: img2,
+    id: 2,
+  },
+
+  {
+    title: "Hindu in Hindu Rashtra",
+    author: "Anand Ranganathan",
+    img: img3,
+    id: 3,
+  },
+];
